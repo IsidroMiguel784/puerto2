e2a5af9af56b82f7c93b78f0d585a73510090591
isidr <isidrohonrubia784@gmail.com>
2023-04-28 11:08:00 +0200

    Versión 0.1 Programa sin modificar para el examen

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import PaqB05.*;
+import PaqB05.interfaz.*;
+
+public class Main {
+    //Proyecto para el examen del trabajo de Java del 28 de Abril de 2023
+    public static void main(String[] args) {
+        VentanaPrincipal principal = new VentanaPrincipal(new Puerto());
+    }
+}
